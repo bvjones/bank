4f4e54ea0d9158604a389b62fbdbfaf9d80b1e3f
bvjones <nanteos@hotmail.com>
2017-03-06 12:08:15 +0000

    remove depricated test for deposit

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -9,9 +9,6 @@ describe Bank do
     end
   end
   describe '#deposit' do
-    it 'allows bank account to recieve a value' do
-      expect(subject).to respond_to(:deposit).with(1).argument
-    end
     it 'allows balance to be updated when depositing money' do
       expect{ subject.deposit(100) }.to change{ subject.balance }.by(100)
     end
