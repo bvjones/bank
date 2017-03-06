7508685efc60a2ca62401edebc6b04068e2f8565
bvjones <nanteos@hotmail.com>
2017-03-06 11:40:39 +0000

    added balance to bank and tested

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,5 +3,9 @@ require 'bank'
 describe Bank do
   let(:bank) { described_class.new }
 
-  
+  describe '#initialise' do
+    it 'should initialise with the balance of 0' do
+      expect(subject.balance).to be(0)
+    end
+  end
 end
